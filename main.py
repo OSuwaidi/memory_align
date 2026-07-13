a0c7e73e1c3d44d551b72df51837c7dca71da89d
osuwaidi <omar.ali.alsuwaidi@gmail.com>
2026-07-13 07:36:54 +0400

    move argparse into main and rearrage

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from torch.optim.lr_scheduler import LinearLR, CosineAnnealingLR, SequentialLR
 from torchvision.transforms import v2
 from sgd import SGD
 import wandb
+import argparse
 import timm
 
 # -------------------------
@@ -37,7 +38,7 @@ def set_seed(seed):
 # Must be defined on the global scope to be picklable and accessible to workers
 def set_worker_seed(worker_id):
     worker_seed = (
-        torch.initial_seed() % 2**32
+            torch.initial_seed() % 2 ** 32
     )  # PyTorch auto increments its seed (internally) to get a unique seed per worker: "torch.initial_seed()" reflects that
     random.seed(worker_seed)
     np.random.seed(worker_seed)
@@ -75,9 +76,9 @@ def train_val(model, opt, epochs, train_loader, val_loader, run, lr_scheduler=No
             best_val_epoch = epoch
 
         run.log(
-            dict(train_loss=round(epoch_loss / n_samples, 2), val_acc=val_acc),
-            step=epoch,
-        )
+                dict(train_loss=round(epoch_loss / n_samples, 2), val_acc=val_acc),
+                step=epoch,
+                )
 
     run.summary["best_val_acc"] = best_val_acc
     run.summary["best_train_loss"] = round(best_train_loss, 2)
@@ -115,158 +116,165 @@ class TransformDataset(Dataset):
         return self.T(x), y
 
 
-def main(
-    *,
-    data_dir: str = "./data",
-    model_name: str = "resnet18",
-    epochs: int = 200,
-    batch_size: int = 256,
-    weight_decay: float = 1e-4,
-):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="./data")
+    parser.add_argument("--arch", type=str, default="resnet18")
+    parser.add_argument("--epochs", type=int, default=200)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--weight_decay", type=float, default=1e-4)
+
+    args, unknown = parser.parse_known_args()  # W&B appends sweep configs into command-line arguments; ignore them and use via "run.config"
+
     train_transform = v2.Compose(
-        [
-            v2.PILToTensor(),
-            v2.RandomCrop(32, padding=4, padding_mode="reflect"),
-            v2.RandomHorizontalFlip(p=0.5),
-            v2.RandAugment(num_ops=2, magnitude=9),
-            v2.ToDtype(torch.float32, scale=True),
-            v2.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-            v2.RandomErasing(p=0.1, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
-        ]
-    )
+            [
+                v2.PILToTensor(),
+                v2.RandomCrop(32, padding=4, padding_mode="reflect"),
+                v2.RandomHorizontalFlip(p=0.5),
+                v2.RandAugment(num_ops=2, magnitude=9),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+                v2.RandomErasing(p=0.1, scale=(0.02, 0.33), ratio=(0.3, 3.3)),
+                ]
+            )
 
     eval_transform = v2.Compose(
-        [
-            v2.PILToTensor(),
-            v2.ToDtype(torch.float32, scale=True),
-            v2.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
-        ]
-    )
+            [
+                v2.PILToTensor(),
+                v2.ToDtype(torch.float32, scale=True),
+                v2.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+                ]
+            )
 
     raw_ds = datasets.CIFAR10(
-        root=data_dir,
-        train=True,
-        download=True,
-    )
+            root=args.data_dir,
+            train=True,
+            download=True,
+            )
     indices = list(range(len(raw_ds)))
 
     train_size = int(0.85 * len(raw_ds))  # 42,500
 
     test_ds = datasets.CIFAR10(
-        root=data_dir,
-        train=False,
-        download=True,
-        transform=eval_transform,
-    )
+            root=args.data_dir,
+            train=False,
+            download=True,
+            transform=eval_transform,
+            )
     test_loader = DataLoader(
-        test_ds,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=1,
-        persistent_workers=False,
-        pin_memory=True,
-    )
+            test_ds,
+            batch_size=500,
+            shuffle=False,
+            num_workers=2,
+            persistent_workers=False,
+            pin_memory=True,
+            )
 
     # Start W&B Sweeps (W&B Sweeps injects the configs automatically):
     run = wandb.init(
-        project="momentum-cifar10",
-        job_type="train",
-        config=dict(
-            model=model_name,
-            epochs=epochs,
-            batch_size=batch_size,
-            weight_decay=weight_decay,
-            mem_align=True,
-        ),
-    )  # individual runs are forced into the parent sweep's project name
+            project="momentum-cifar10",
+            job_type="train",
+            config=dict(
+                    model=args.arch,
+                    epochs=args.epochs,
+                    batch_size=args.batch_size,
+                    weight_decay=args.weight_decay,
+                    ema=False,
+                    ),
+            )  # individual runs are forced into the parent sweep's project name
 
     config = run.config
-    ema = config.ema
+    mem_align = config.mem_align
+    absorb = config.absorb
     couple = config.couple
-    per = config.per
+    tau = config.tau
     lr = config.lr
     seed = config.seed
 
-    run.name = f"mem_ema:{ema}_coup:{couple}_per:{per}_{lr}_{seed}"
+    f = lambda truth: str(truth)[0]
+
+    run.name = f"mem:{f(mem_align)}_abs:{f(absorb)}_coup:{f(couple)}_tau:{tau}_{lr}_{seed}"
 
     set_seed(seed)
 
-    if model_name == "resnet18":
+    if args.arch == "resnet18":
         model = resnet18(
-            norm_layer=lambda n_channels: nn.GroupNorm(
-                num_groups=min(32, n_channels // 4), num_channels=n_channels
-            )
-        )
+                norm_layer=lambda n_channels: nn.GroupNorm(
+                        num_groups=min(32, n_channels // 4), num_channels=n_channels
+                        )
+                )
         model.fc = nn.Linear(512, len(raw_ds.classes), bias=True)
-    else:  # "repvgg_b0", "nf_resnet26", etc.
+    else:
         model = timm.create_model(
-            model_name, pretrained=False, num_classes=len(raw_ds.classes), drop_rate=0.0
-        )
+                args.arch, pretrained=False, num_classes=len(raw_ds.classes), drop_rate=0.0
+                )
 
     model.to(DEVICE)
 
     train_indices, val_indices = train_test_split(
-        indices, train_size=train_size, stratify=raw_ds.targets, random_state=seed
-    )
+            indices, train_size=train_size, stratify=raw_ds.targets, random_state=seed
+            )
 
     train_ds, val_ds = Subset(raw_ds, train_indices), Subset(raw_ds, val_indices)
     train_ds, val_ds = (
         TransformDataset(train_ds, train_transform),
         TransformDataset(val_ds, eval_transform),
-    )
+        )
 
     train_loader = DataLoader(
-        train_ds,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=NUM_WORKERS,  # torch pickles "worker_init_fn" + dataset + all its transforms and sends serialized copy to each worker
-        persistent_workers=NUM_WORKERS > 0,
-        pin_memory=True,
-        drop_last=True,  # a final small batchsize (4 here) is too noisy and can throw the model off, especially with BatchNorm
-        worker_init_fn=set_worker_seed,
-        generator=torch.Generator().manual_seed(seed),
-    )
+            train_ds,
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=NUM_WORKERS,  # torch pickles "worker_init_fn" + dataset + all its transforms and sends serialized copy to each worker
+            persistent_workers=NUM_WORKERS > 0,
+            pin_memory=True,
+            drop_last=True,  # a final small batchsize (4 here) is too noisy and can throw the model off, especially with BatchNorm
+            worker_init_fn=set_worker_seed,
+            generator=torch.Generator().manual_seed(seed),
+            )
 
     val_loader = DataLoader(
-        val_ds,
-        batch_size=500,
-        shuffle=False,
-        num_workers=1,
-        persistent_workers=False,
-        pin_memory=True,
-    )
+            val_ds,
+            batch_size=500,
+            shuffle=False,
+            num_workers=2,
+            persistent_workers=False,
+            pin_memory=True,
+            )
 
     optimizer = SGD(
-        model.parameters(),
-        lr=lr,
-        weight_decay=weight_decay,
-        EMA=ema,
-        couple=couple,
-        per=per,
-    )
+            model.parameters(),
+            lr=lr,
+            mem_align=mem_align,
+            EMA=False,
+            couple=couple,
+            weight_decay=args.weight_decay,
+            absorb=absorb,
+            tau=tau,
+            )
 
     steps_per_epoch = len(train_loader)
-    total_steps = steps_per_epoch * epochs
+    total_steps = steps_per_epoch * args.epochs
     warmup_steps = steps_per_epoch * WARMUP_EPOCHS
 
     warmup_scheduler = LinearLR(
-        optimizer, start_factor=0.01, end_factor=1.0, total_iters=warmup_steps
-    )
+            optimizer, start_factor=0.01, end_factor=1.0, total_iters=warmup_steps
+            )
 
     cosine_scheduler = CosineAnnealingLR(
-        optimizer, T_max=(total_steps - warmup_steps), eta_min=1e-6
-    )
+            optimizer, T_max=(total_steps - warmup_steps), eta_min=1e-6
+            )
 
     # Combine schedulers sequentially at the iteration level
     scheduler = SequentialLR(
-        optimizer,
-        schedulers=[warmup_scheduler, cosine_scheduler],
-        milestones=[warmup_steps],
-    )
+            optimizer,
+            schedulers=[warmup_scheduler, cosine_scheduler],
+            milestones=[warmup_steps],
+            )
 
     best_model = train_val(
-        model, optimizer, epochs, train_loader, val_loader, run, lr_scheduler=scheduler
-    )
+            model, optimizer, args.epochs, train_loader, val_loader, run, lr_scheduler=scheduler
+            )
 
     model.load_state_dict(best_model)
     test_acc = eval_model(model, test_loader)
@@ -277,17 +285,4 @@ def main(
 
 
 if __name__ == "__main__":
-    # To run: $ CUDA_VISIBLE_DEVICES=0 uv run wandb agent --forward-signals <entity/project/sweep_id> &
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="./data")
-    parser.add_argument("--model_name", type=str, default="resnet18")
-    parser.add_argument("--epochs", type=int, default=200)
-    parser.add_argument("--batch_size", type=int, default=256)
-    parser.add_argument("--weight_decay", type=float, default=1e-4)
-
-    args, unknown = (
-        parser.parse_known_args()
-    )  # W&B appends sweep configs into command-line arguments; ignore them and use via "run.config"
-    raise SystemExit(main(**vars(args)))
+    raise SystemExit(main())
